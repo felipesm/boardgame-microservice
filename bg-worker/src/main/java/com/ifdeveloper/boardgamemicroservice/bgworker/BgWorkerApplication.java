6f78055a679a4753b6713dc682656d7e905bcd23
Felipe Maia <felipe.silwa@gmail.com>
2021-07-06 04:04:20 -0300

    Projeto bg-worker

---
+++ bg-worker/src/main/java/com/ifdeveloper/boardgamemicroservice/bgworker/BgWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.ifdeveloper.boardgamemicroservice.bgworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgWorkerApplication.class, args);
+	}
+
+}
---
+++ bg-worker/src/test/java/com/ifdeveloper/boardgamemicroservice/bgworker/BgWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifdeveloper.boardgamemicroservice.bgworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
