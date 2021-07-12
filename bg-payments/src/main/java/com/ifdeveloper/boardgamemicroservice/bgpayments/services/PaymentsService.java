ef6fed24c095babae38c3c65bb70ae1ded4f1595
Felipe Maia <felipe.silwa@gmail.com>
2021-07-11 23:52:51 -0300

    Implementação básica para comunicação entre serviços

--- bg-payments/src/main/java/com/ifdeveloper/boardgamemicroservice/bgpayments/services/PaymentsService.java
+++ bg-payments/src/main/java/com/ifdeveloper/boardgamemicroservice/bgpayments/services/PaymentsService.java
@@ -1,13 +1,32 @@
 package com.ifdeveloper.boardgamemicroservice.bgpayments.services;
 
-import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.ifdeveloper.boardgamemicroservice.bgpayments.entities.BoardGame;
 import com.ifdeveloper.boardgamemicroservice.bgpayments.entities.Payments;
 
 @Service
 public class PaymentsService {
+	
+	@Value("${bg-worker.host}")
+	private String bgWorkerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payments getPayments(long idBoardGame, int rentDays) {
-		return new Payments("Aluguel Carcassonne", 25.00, rentDays);
+		
+		Map<String, String> parametersRequest = new HashMap<>();
+		parametersRequest.put("id", String.valueOf(idBoardGame));
+		String url = String.format("%s/boardgames/{id}", bgWorkerHost);
+		
+		BoardGame boardGame = restTemplate.getForObject(url, BoardGame.class, parametersRequest);
+		return new Payments(boardGame.getName(), boardGame.getRentValue(), rentDays);
 	}
 }
