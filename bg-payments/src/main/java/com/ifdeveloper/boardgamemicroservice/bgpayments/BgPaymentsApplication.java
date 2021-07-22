53a62bd53ff27949f9208a30005c9e51635b5e41
Felipe Maia <felipe.silwa@gmail.com>
2021-07-22 02:03:12 -0300

    Configurando eureka client

--- bg-payments/src/main/java/com/ifdeveloper/boardgamemicroservice/bgpayments/BgPaymentsApplication.java
+++ bg-payments/src/main/java/com/ifdeveloper/boardgamemicroservice/bgpayments/BgPaymentsApplication.java
@@ -2,10 +2,10 @@ package com.ifdeveloper.boardgamemicroservice.bgpayments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "bg-worker")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class BgPaymentsApplication {
