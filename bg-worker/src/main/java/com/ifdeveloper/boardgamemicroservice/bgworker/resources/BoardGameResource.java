a4e648d5da6071d0bf9d6639aed4f61c81ecbcfe
Felipe Maia <felipe.silwa@gmail.com>
2021-07-24 02:49:01 -0300

    Configurando bg-worker como cliente do bg-config-server

--- bg-worker/src/main/java/com/ifdeveloper/boardgamemicroservice/bgworker/resources/BoardGameResource.java
+++ bg-worker/src/main/java/com/ifdeveloper/boardgamemicroservice/bgworker/resources/BoardGameResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import com.ifdeveloper.boardgamemicroservice.bgworker.repositories.BoardGameRepo
 public class BoardGameResource {
 	
 	private static Logger log = LoggerFactory.getLogger(BoardGameResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 
 	@Autowired
 	private Environment env;
@@ -42,5 +46,11 @@ public class BoardGameResource {
 		
 		BoardGame boardGame = repository.findById(id).get();
 		return ResponseEntity.ok(boardGame);
+	}
+	
+	@GetMapping(value = "/config")
+	public ResponseEntity<Void> getConfig() {
+		log.info("[bg-worker] /config - {}", testConfig);
+		return ResponseEntity.noContent().build();
 	}
 }
