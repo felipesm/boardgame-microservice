bd562b0cb9b1f08f2e05889e7c9491054c0c55f3
Felipe Maia <felipe.silwa@gmail.com>
2021-07-24 16:50:50 -0300

    Criação projeto bg-oauth

---
+++ bg-oauth/src/test/java/com/ifdeveloper/boardgamemicroservice/bgoauth/BgOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifdeveloper.boardgamemicroservice.bgoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
