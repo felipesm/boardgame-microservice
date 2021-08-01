35f3f80dfbdd27ee60604db7313a01432a02823c
Felipe Maia <felipe.silwa@gmail.com>
2021-08-01 02:17:09 -0300

    Configuração de database pelo bg-config-server

--- bg-worker/src/main/java/com/ifdeveloper/boardgamemicroservice/bgworker/resources/BoardGameResource.java
+++ bg-worker/src/main/java/com/ifdeveloper/boardgamemicroservice/bgworker/resources/BoardGameResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +23,6 @@ public class BoardGameResource {
 	
 	private static Logger log = LoggerFactory.getLogger(BoardGameResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-
 	@Autowired
 	private Environment env;
 	
@@ -50,9 +46,4 @@ public class BoardGameResource {
 		return ResponseEntity.ok(boardGame);
 	}
 	
-	@GetMapping(value = "/config")
-	public ResponseEntity<Void> getConfig() {
-		log.info("[bg-worker] /config - {}", testConfig);
-		return ResponseEntity.noContent().build();
-	}
 }
